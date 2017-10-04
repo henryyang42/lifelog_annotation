0f7e9978c3d5530a6d3dcf0b724618216d4a3215
HenryYang42 <henryyang42@gmail.com>
2017-10-04 23:20:29 +0800

    Finish diaries selection

--- scripts/create_accounts.py
+++ scripts/create_accounts.py
@@ -4,6 +4,8 @@ from access_django import *
 from django.contrib.auth.models import User
 from annotate.models import *
 
+golden_standard_id = [53, 109, 144, 200, 203, 230, 255, 256, 317, 368, 398, 413, 421, 464, 525, 533, 558, 562, 572, 573, 576, 638, 657, 712, 730, 747, 750, 765, 811, 931, 936, 968, 990, 1029, 1035, 1190, 1229, 1280, 1295, 1380, 1449, 1465, 1475, 1518, 1586, 1596, 1643, 1653, 1701, 1740, 1801, 1879, 1922, 1943, 1983, 2044, 2087, 2108, 2129, 2132, 2155, 2260, 2334, 2360, 2407, 2424, 2446, 2461, 2510, 2551, 2648, 2670, 2689, 2746, 2883, 2964, 2965, 2988, 2997, 3004, 3083, 3188, 3342, 3409, 3439, 3454, 3527, 3694, 3815, 3913, 3973, 4399, 4444, 4478, 4489, 4547, 4566, 4609, 4662, 4664]
+
 
 def random_password(N=10):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(N))
@@ -21,7 +23,7 @@ def write_account_info(username, password, file):
 
 def get_entries():
     entries = Entry.objects.all()
-    golden_entries = entries[:100]
+    golden_entries = entries.filter(source_id__in=golden_standard_id)
     for entry in golden_entries:
         entry.golden = True
         entry.save()
